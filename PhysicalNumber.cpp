cea36310ba4ff71ff644c07b7640b6b89028f248
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-04-10 20:42:45 +0300

    Update PhysicalNumber.cpp

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -130,7 +130,8 @@ std::istream& ariel::operator>>(std::istream& is, PhysicalNumber& other)
 {
     ios::pos_type startPosition = is.tellg();
     std:string s;
- if ( (!(is >> other._val))               ||
+    int val=0;;
+ if ( (!(is >> val))               ||
          (!getAndCheckNextCharIs(is,'[')) ||
          (!(is >> s))                     ||
          (!(getAndCheckNextCharIs(is,']')))) {
@@ -140,25 +141,28 @@ std::istream& ariel::operator>>(std::istream& is, PhysicalNumber& other)
         cout << s;       
 
         Unit new_type;
-        if( s.compare("km") == 0 ) new_type = Unit::KM; 
-        else if( s.compare("m") == 0 ) new_type = Unit::M; 
-        else if( s.compare("cm") == 0 ) new_type = Unit::CM; 
+            if(!s.empty()){
+                if( s.compare("km") == 0 ) new_type = Unit::KM; 
+                else if( s.compare("m") == 0 ) new_type = Unit::M; 
+                else if( s.compare("cm") == 0 ) new_type = Unit::CM; 
 
-        else if( s.compare("ton") == 0 ) new_type = Unit::TON; 
-        else if( s.compare("kg") == 0 ) new_type = Unit::KG; 
-        else if( s.compare("g") == 0 ) new_type = Unit::G; 
+                else if( s.compare("ton") == 0 ) new_type = Unit::TON; 
+                else if( s.compare("kg") == 0 ) new_type = Unit::KG; 
+                else if( s.compare("g") == 0 ) new_type = Unit::G; 
 
-        else if( s.compare("hour") == 0 ) new_type = Unit::HOUR; 
-        else if( s.compare("min") == 0 ) new_type = Unit::MIN; 
-        else if( s.compare("sec") == 0 ) new_type = Unit::SEC; 
-        else{
-         other._val=0;
-        auto errorState = is.rdstate(); // remember error state
-        is.clear(); // clear error so seekg will work
-        is.seekg(startPosition); // rewind
-        is.clear(errorState); // set back the error flag
-        }
-        other._name = new_type;
+                else if( s.compare("hour") == 0 ) new_type = Unit::HOUR; 
+                else if( s.compare("min") == 0 ) new_type = Unit::MIN; 
+                else if( s.compare("sec") == 0 ) new_type = Unit::SEC; 
+                else{
+
+                auto errorState = is.rdstate(); // remember error state
+                is.clear(); // clear error so seekg will work
+                is.seekg(startPosition); // rewind
+                is.clear(errorState); // set back the error flag
+                return is;
+                }
+            }
+
          }
          else {
             auto errorState = is.rdstate(); // remember error state
@@ -167,6 +171,7 @@ std::istream& ariel::operator>>(std::istream& is, PhysicalNumber& other)
             is.clear(errorState); // set back the error flag
         return is;
          }
+         
         
         return is;
 }
