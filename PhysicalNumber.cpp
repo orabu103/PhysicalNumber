9773219c4d1987778431393f3fc39a5edd9e2e4d
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-04-10 19:12:15 +0300

    Update input

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -141,8 +141,10 @@ std::istream& ariel::operator>>(std::istream& is, PhysicalNumber& other)
         is.clear(); // clear error so seekg will work
         is.seekg(startPosition); // rewind
         is.clear(errorState); // set back the error flag
-
-        Unit new_type;
+        return is;
+     
+        }
+           Unit new_type;
         if( s.compare("km") == 0 ) new_type = Unit::KM; 
         else if( s.compare("m") == 0 ) new_type = Unit::M; 
         else if( s.compare("cm") == 0 ) new_type = Unit::CM; 
@@ -155,13 +157,13 @@ std::istream& ariel::operator>>(std::istream& is, PhysicalNumber& other)
         else if( s.compare("min") == 0 ) new_type = Unit::MIN; 
         else if( s.compare("sec") == 0 ) new_type = Unit::SEC; 
         else{
-                    auto errorState = is.rdstate(); // remember error state
+        auto errorState = is.rdstate(); // remember error state
         is.clear(); // clear error so seekg will work
         is.seekg(startPosition); // rewind
         is.clear(errorState); // set back the error flag
+        return is;
         }
         other._name = new_type;
-        }
         return is;
 }
 // Checking:                                                              
